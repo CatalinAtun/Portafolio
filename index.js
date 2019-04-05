a9e6c53977c240585563a791b0a2045d6d98f1c8
Catalina Campos <catalina.camposcaceres9@gmail.com>
2019-04-04 23:40:35 -0300

    Intento de scale que no funciona, ayuda

---
+++ index.js
@@ -0,0 +1,13 @@
+/* window.onload = (
+    function wAndH() {
+        let razon = screen.width / screen.height;
+        let hola = 1.175 / razon
+        let hola2 = 'scale('+ hola + ')';
+        console.log(hola2)
+        document.documentElement.style
+            .setProperty('transform', hola2)
+    }
+) */
+/* 1340 / 640 = 2.09 */
+/* 1366 / 768 = 1.175*/
+
